bdb469d10c785b1f30fe2b5a9542c11fe2e03a2b
YUapisit <88958116+Apisit250aps@users.noreply.github.com>
2024-01-03 12:37:45 +0700

    models migrations

--- store/models.py
+++ store/models.py
@@ -1,3 +1,126 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class ProductType(models.Model):
+    # property
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    # timestamp
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        
+        return self.name
+    
+class ProductCategory(models.Model):
+    # property
+    id = models.BigAutoField(primary_key=True)
+    type = models.ForeignKey(ProductType, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    # timestamp
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        
+        return self.name
+    
+class Area(models.Model):
+    # property
+    id = models.BigAutoField(primary_key=True)
+    code = models.CharField(max_length=2, unique=True, blank=True)
+    name = models.CharField(max_length=255)
+    # timestamp
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Shop(models.Model):
+    # property
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    area = models.ForeignKey(Area, on_delete=models.CASCADE)
+    product_type = models.ForeignKey(ProductType, on_delete=models.CASCADE)
+    # information
+    name = models.CharField(max_length=255)
+    code = models.CharField(max_length=8, unique=True, blank=True)
+    contact = models.CharField(max_length=255, blank=True)
+    tel = models.CharField(max_length=10, blank=True)
+    fax = models.CharField(max_length=10, blank=True)
+    email = models.EmailField(max_length=255, blank=True)
+    remark = models.TextField(blank=True)
+    # address
+    province = models.CharField(max_length=255, blank=True)
+    district = models.CharField(max_length=255, blank=True)
+    sub_district = models.CharField(max_length=255, blank=True)
+    address = models.TextField(blank=True)
+    # timestamp
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    # property
+    id = models.BigAutoField(primary_key=True)
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    # information
+    code = models.CharField(max_length=8, unique=True)
+    name = models.CharField(max_length=255)
+    desc = models.TextField()
+    unit = models.IntegerField()
+    cost = models.DecimalField(max_digits=9, decimal_places=2)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    # timestamp
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+    
+class ProductDelete(models.Model):
+    # property
+    id = models.BigAutoField(primary_key=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    unit = models.IntegerField()
+    # timestamp
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.product
+    
+class InputInvoice(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    shop = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    invoice_no = models.CharField(max_length=16)
+    total_price = models.DecimalField(max_digits=9, decimal_places=2)
+    discount = models.DecimalField(max_digits=9, decimal_places=2)
+    remark = models.TextField(blank=True)
+    # timestamp 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.invoice_no
+    
+class InputData(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    invoice = models.ForeignKey(InputInvoice, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    unit_price = models.DecimalField(max_digits=9, decimal_places=2)
+    discount = models.DecimalField(max_digits=9, decimal_places=2)
+    # timestamp 
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.product
